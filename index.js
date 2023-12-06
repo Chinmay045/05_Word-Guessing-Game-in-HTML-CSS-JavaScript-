fa21d7521bb522fd7c866b2c0496a69c52943468
Chinmay <chinz.sandur07@gmail.com>
2023-12-06 16:41:04 +0530

    add display logic

--- index.js
+++ index.js
@@ -201,11 +201,19 @@ const wordList = [
 
 
 
+const inputs = document.querySelector(".inputs");
 
-
-function randomWord(){
+function randomWord() {
     let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
-    console.log(ranObj); 
+    let word = ranObj.word;
+    console.log(word);
+
+    let html = "";
+    for (let i = 0; i < word.length; i++) {
+        html += `<input type="text" value="" disabled>
+        `
+    }
+    inputs.innerHTML = html;
 }
 
 randomWord()
